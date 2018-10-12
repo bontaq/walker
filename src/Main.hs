c8b3294265aeacca68348b6cb756845c261a093d
Ian Davidson <bontaq@gmail.com>
2018-10-11 20:27:40 -0400

    eh

--- src/Main.hs
+++ src/Main.hs
@@ -5,6 +5,15 @@ import System.Directory
 data Files = Name String
            | Directory String [Files]
 
+data File = File String [String]
+
+target :: [Char]
+target = ""
+
+-- alright, maybe the best idea is to start with a tree
+-- of all files in the project.  then, build a second tree
+-- based off parsing the files (starting with the target)
+
 main :: IO ()
 main = do
   putStrLn "hello world"
